b0a4734dd0ed959debdab6867427eb43104353c9
Brendan Mac Donagh <brendan.macdonagh@arekibo.com>
2018-05-18 17:43:19 +0100

    First commit and Jekyll test

---
+++ gulpfile.js
@@ -0,0 +1,131 @@
+// gulpfile.js
+
+// Define variables.
+var autoprefixer = require('autoprefixer');
+var browserSync  = require('browser-sync').create();
+var cleancss     = require('gulp-clean-css');
+var concat       = require('gulp-concat');
+var del          = require('del');
+var gulp         = require('gulp');
+var gutil        = require('gulp-util');
+var imagemin     = require('gulp-imagemin');
+var notify       = require('gulp-notify');
+var postcss      = require('gulp-postcss');
+var rename       = require('gulp-rename');
+var run          = require('gulp-run');
+var runSequence  = require('run-sequence');
+var sass         = require('gulp-ruby-sass');
+var uglify       = require('gulp-uglify');
+
+// Include paths file.
+var paths = require('./_assets/gulp_config/paths');
+
+// Processes SCSS.
+gulp.task('build:styles:main', function() {
+    // Compile SCSS, run autoprefixer, and minify CSS.
+    return sass(paths.sassFiles + '/main.scss', {
+        style: 'compressed',
+        trace: true,
+        loadPath: [paths.sassFiles]
+    }).pipe(postcss([ autoprefixer({ browsers: ['last 2 versions'] }) ]))
+        .pipe(cleancss())
+        .pipe(gulp.dest(paths.jekyllCssFiles))
+        .pipe(gulp.dest(paths.siteCssFiles))
+        .pipe(browserSync.stream())
+        .on('error', gutil.log);
+});
+
+// Creates critical CSS file.
+gulp.task('build:styles:critical', function() {
+    // Compile critical SCSS rules, run autoprefixer, minify CSS, and place in
+    // appropriate location so it can be inlined in the HTML head.
+    return sass(paths.sassFiles + '/critical.scss', {
+        style: 'compressed',
+        trace: true,
+        loadPath: [paths.sassFiles]
+    }).pipe(postcss([ autoprefixer({ browsers: ['last 2 versions'] }) ]))
+        .pipe(cleancss())
+        .pipe(gulp.dest('_includes'))
+        .on('error', gutil.log);
+});
+
+// Builds all styles.
+gulp.task('build:styles', ['build:styles:main', 'build:styles:critical']);
+
+// Deletes CSS.
+gulp.task('clean:styles', function(callback) {
+    del([paths.jekyllCssFiles + 'main.css',
+        paths.siteCssFiles + 'main.css',
+        '_includes/critical.css'
+    ]);
+    callback();
+});
+
+// Processes JS.
+gulp.task('build:scripts', function() {
+    // Concatenates and uglifies global JS files and outputs result to the
+    // appropriate location.
+    return gulp.src([
+        paths.jsFiles + '/global/lib' + paths.jsPattern,
+        paths.jsFiles + '/global/*.js'
+    ])
+        .pipe(concat('main.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest(paths.jekyllJsFiles))
+        .pipe(gulp.dest(paths.siteJsFiles))
+});
+
+// Deletes processed JS.
+gulp.task('clean:scripts', function(callback) {
+    del([paths.jekyllJsFiles + 'main.js', paths.siteJsFiles + 'main.js']);
+    callback();
+});
+
+// Optimizes and copies image files.
+gulp.task('build:images', function() {
+    return gulp.src(paths.imageFilesGlob)
+        .pipe(imagemin())
+        .pipe(gulp.dest(paths.jekyllImageFiles))
+        .pipe(gulp.dest(paths.siteImageFiles))
+        .pipe(browserSync.stream());
+});
+
+// Deletes processed images.
+gulp.task('clean:images', function(callback) {
+    del([paths.jekyllImageFiles, paths.siteImageFiles]);
+    callback();
+});
+
+// Runs jekyll build command.
+gulp.task('build:jekyll', function() {
+    var shellCommand = 'bundle exec jekyll build --config _config.yml';
+
+    return gulp.src('')
+        .pipe(run(shellCommand))
+        .on('error', gutil.log);
+});
+
+// Deletes the entire _site directory.
+gulp.task('clean:jekyll', function(callback) {
+    del(['_site']);
+    callback();
+});
+
+// Main clean task.
+// Deletes _site directory and processed assets.
+gulp.task('clean', ['clean:jekyll',
+    'clean:fonts',
+    'clean:images',
+    'clean:scripts',
+    'clean:styles']);
+
+// Builds site anew.
+gulp.task('build', function(callback) {
+    runSequence('clean',
+        ['build:scripts', 'build:images', 'build:styles', 'build:fonts'],
+        'build:jekyll',
+        callback);
+});
+
+// Default Task: builds site.
+gulp.task('default', ['build']);
